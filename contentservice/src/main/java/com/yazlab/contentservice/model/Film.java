78374fda71e3888ba8c07db6bcf0828247080b50
YEKTA34 <cengizyekta195@gmail.com>
2026-03-23 22:42:28 +0300

    Kişi 2: Film.java model dosyası lombok'tan arındırıldı ve manuel constructor/metotlar eklendi

--- contentservice/src/main/java/com/yazlab/contentservice/model/Film.java
+++ contentservice/src/main/java/com/yazlab/contentservice/model/Film.java
@@ -2,13 +2,7 @@ package com.yazlab.contentservice.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Document(collection = "films") 
 public class Film {
     @Id
@@ -18,4 +12,34 @@ public class Film {
     private int yil;
     private String yonetmen;
     private double puan;
+
+    public Film() {
+    }
+
+    public Film(String id, String filmAdi, String tur, int yil, String yonetmen, double puan) {
+        this.id = id;
+        this.filmAdi = filmAdi;
+        this.tur = tur;
+        this.yil = yil;
+        this.yonetmen = yonetmen;
+        this.puan = puan;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getFilmAdi() { return filmAdi; }
+    public void setFilmAdi(String filmAdi) { this.filmAdi = filmAdi; }
+
+    public String getTur() { return tur; }
+    public void setTur(String tur) { this.tur = tur; }
+
+    public int getYil() { return yil; }
+    public void setYil(int yil) { this.yil = yil; }
+
+    public String getYonetmen() { return yonetmen; }
+    public void setYonetmen(String yonetmen) { this.yonetmen = yonetmen; }
+
+    public double getPuan() { return puan; }
+    public void setPuan(double puan) { this.puan = puan; }
 }
