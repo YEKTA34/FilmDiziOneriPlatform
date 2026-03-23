9aabb352701421e29bb3b41fddbf9bb04d2ad133
YEKTA34 <cengizyekta195@gmail.com>
2026-03-23 21:59:24 +0300

    Kişi 2: Review.java model dosyası lombok'tan arındırıldı ve manuel metotlar eklendi

--- reviewservice/src/main/java/com/yazlab/reviewservice/model/Review.java
+++ reviewservice/src/main/java/com/yazlab/reviewservice/model/Review.java
@@ -2,15 +2,23 @@ package com.yazlab.reviewservice.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.Data;
 
-@Data
 @Document(collection = "reviews")
 public class Review {
     @Id
     private String id;
-    private String filmId;     
+    private String filmId;
     private String kullaniciAdi;
     private String yorumMetni;
     private double puan;
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getFilmId() { return filmId; }
+    public void setFilmId(String filmId) { this.filmId = filmId; }
+    public String getKullaniciAdi() { return kullaniciAdi; }
+    public void setKullaniciAdi(String kullaniciAdi) { this.kullaniciAdi = kullaniciAdi; }
+    public String getYorumMetni() { return yorumMetni; }
+    public void setYorumMetni(String yorumMetni) { this.yorumMetni = yorumMetni; }
+    public double getPuan() { return puan; }
+    public void setPuan(double puan) { this.puan = puan; }
 }
